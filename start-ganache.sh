d166b43757a4e5772386fa6d70d2e568768baca9
driemworks <tonyrriemer@gmail.com>
2018-09-16 12:09:43 -0400

    updating startup scripts

--- start-ganache.sh
+++ start-ganache.sh
@@ -1,3 +1,9 @@
+# start ganache
+echo "Starting ganache on port 7545"
+npm install -g ganache-cli
+nohup ganache-cli --port 7545 -a 5 &>/dev/null &
+#--account="<0xaaaaaaaa1>, 100" [--account="<0xaaaaaaaa2>, 100"] --secure --unlock "0xaaaaaaaa1" --unlock "0xaaaaaaaa2"
+
 # start mongo db
 echo "Starting MongoDB on port 27017"
 nohup mongod &>/dev/null &
